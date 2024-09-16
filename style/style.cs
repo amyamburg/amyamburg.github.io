23c3cb40897e712c876c88c48b7ec32cde4f4f9e
Burgess, Amy <ab407@students.waikato.ac.nz>
2024-09-16 21:17:03 +1200

    Style added. image and div sizes.

---
+++ style/style.cs
@@ -0,0 +1,49 @@
+header {
+    background-color: #f9e0ad
+    color: white;
+    padding: 20px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+}
+
+.logo-img {
+    height: 50px; /* small logo */
+    margin-right: 15px; /* put space between the title and logo */
+}
+ul{
+    list-style-type: none; /*remove the bullet points*/
+}
+.art-img-div{
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+.art-img{
+    width:100%;
+    max-width: 800px;
+    height: auto; 
+    display: block;
+}
+
+.map-img {
+    height: 100px; /* smaller map */
+
+}
+footer{
+    background-color: #f9e0ad
+    padding: 20px;
+}
+.footer-div{
+    display: flex;
+    justify-content: space-between; /* Space out buttons to the edges */
+    align-items: center; /* Center buttons vertically */
+}
+.footer-btn{
+    background-color: #ffaf0c
+    padding: 10px 20px;
+    text-align: center;
+    border-radius: 4px;
+}
+.footer-btn:hover {
+    background-color: #d99200 ; /* Darker colour when hover */
+}
