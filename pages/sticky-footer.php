34e7554c44430e34ad8b995ba103c8fe308d2fd2
Burgess, Amy <ab407@students.waikato.ac.nz>
2024-10-03 15:03:41 +1300

    using php for the sticky footer?

---
+++ pages/sticky-footer.php
@@ -0,0 +1,17 @@
+<?php
+?>
+  <!-- audio and buttons -->
+  <div class="stickToBottom">
+   <button class="footer-btn" id="prev-button">&#8249;</button>
+
+   <!-- Audio file.-->
+   <audio id="audioPlayer">
+      <source src="resources/UoW_Tga_GivenRecording.mp3" type="audio/mp3">
+  </audio>
+  <div class="custom-controls">
+      <button class="control-btn" id="playPauseBtn">&#9658;</button>
+      <input type="range" id="seekBar" value="0" step="1" min="0">
+  </div>
+  <button class="footer-btn" id="next-button">&#8250;</button>
+
+
