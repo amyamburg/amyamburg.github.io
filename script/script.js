58137aef2e1fda1dd59f783bb01146ba87cd3828
ShiLlama <sg218@students.waikato.ac.nz>
2024-10-04 22:36:15 +1300

    Deleted it for now since it wasn't working in my favour. Will try again later

--- script/script.js
+++ script/script.js
@@ -147,36 +147,3 @@ function navigateToPage() {
     window.location.href = pageUrl;
 }
 
-//==========================================================================================
-//Carousel functionality 
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slides");
-    let dots = document.getElementsByClassName("dot");
-
-    if (n > slides.length) {slideIndex = 1;}
-    if (n < 1) {slideIndex = slides.length;}
-
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-}
